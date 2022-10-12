3787fff2bd5279c8a2f12696692f0a60f5b83cb2
Reem <apple@Apples-MacBook-Pro.local>
2022-10-12 16:49:16 +0600

    Final

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                     <Navbar.Brand>Web Dev Quiz</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto justify-content-end">
+                        <Nav className="me-auto">
                             <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
                             <Nav.Link as={Link} to={'/topics'}>Quiz Topics</Nav.Link>
                             <Nav.Link as={Link} to={'/statistics'}>Chart</Nav.Link>
